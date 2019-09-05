35b54ed7f2f0f7a89f6957f75d0e1fd4ac32af78
Kintaro-Oe <marcusclairmont@gmail.com>
2019-09-05 11:02:35 +0100

    Initial commit

---
+++ navigator.py
@@ -0,0 +1,7 @@
+def locate(directions):
+    # iterate over the string one char at a time.
+    # if char = ( then floor += 1
+    # if char =) then floor -= 1
+    # floor can be any positive or negative number
+    
+    return 0
---
+++ test_navigator.py
@@ -0,0 +1,17 @@
+import unittest
+import navigator
+
+class testNavigationMethods(unittest.TestCase):
+
+    def test_locate(self):
+        self.assertEqual(navigator.locate('()'), 0)
+
+# acceptance criteria
+
+        # self.assertEqual(navigator.locate('(())'), 0)
+        # self.assertEqual(navigator.locate('()()'), 0)
+        # self.assertEqual(navigator.locate('((('), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
