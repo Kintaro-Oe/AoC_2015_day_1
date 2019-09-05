6d981472da3951531addaac01ba2568331f7831c
Kintaro-Oe <marcusclairmont@gmail.com>
2019-09-05 11:27:42 +0100

    Quick refactor

--- navigator.py
+++ navigator.py
@@ -1,9 +1,4 @@
 def locate(directions):
-    # iterate over the string one char at a time.
-    # if char = ( then floor += 1
-    # if char =) then floor -= 1
-    # floor can be any positive or negative number
-
     floor = 0
 
     for char in directions:
--- test_navigator.py
+++ test_navigator.py
@@ -5,20 +5,24 @@ class testNavigationMethods(unittest.TestCase):
 
     def test_locate(self):
         self.assertEqual(navigator.locate(''), 0)
+
+        # test floor increments
         self.assertEqual(navigator.locate('('), 1)
         self.assertEqual(navigator.locate('(('), 2)
+
+        # test floor decrements
         self.assertEqual(navigator.locate(')'), -1)
         self.assertEqual(navigator.locate('))'), -2)
 
-
-
-
-# acceptance criteria
-
-        # self.assertEqual(navigator.locate('(())'), 0)
-        # self.assertEqual(navigator.locate('()()'), 0)
-        # self.assertEqual(navigator.locate('((('), 3)
-
+        # acceptance criteria
+        self.assertEqual(navigator.locate('(())'), 0)
+        self.assertEqual(navigator.locate('()()'), 0)
+        self.assertEqual(navigator.locate('((('), 3)
+        self.assertEqual(navigator.locate('))((((('), 3)
+        self.assertEqual(navigator.locate('())'), -1)
+        self.assertEqual(navigator.locate('))('), -1)
+        self.assertEqual(navigator.locate(')))'), -3)
+        self.assertEqual(navigator.locate(')())())'), -3)
 
 if __name__ == '__main__':
     unittest.main()
